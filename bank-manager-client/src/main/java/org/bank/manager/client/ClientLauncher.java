4c424226d68b9b9a683514190a8aad0028a7a7da
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2020-07-08 11:57:28 +0200

    exercise_10

--- bank-manager-client/src/main/java/org/bank/manager/client/ClientLauncher.java
+++ bank-manager-client/src/main/java/org/bank/manager/client/ClientLauncher.java
@@ -2,6 +2,7 @@ package org.bank.manager.client;
 
 import com.ontimize.gui.ApplicationLauncher;
 import com.ontimize.gui.ApplicationToolBar;
+import com.ontimize.permission.PermissionButton;
 import com.ontimize.util.math.MathExpressionParser;
 import com.ontimize.util.math.MathExpressionParserFactory;
 
@@ -9,6 +10,7 @@ public class ClientLauncher {
 
 	public static void main(String[] args) {
 		ApplicationToolBar.DEFAULT_BUTTON_SIZE=30;
+		PermissionButton.setPropertyFile("org/bank/manager/client/permission/PermissionButton.properties");
 		System.setProperty(MathExpressionParserFactory.MATH_EXPRESSION_PARSER_PROPERTY, MathExpressionParser.MESP);
 		ApplicationLauncher.main(args);
 	}
