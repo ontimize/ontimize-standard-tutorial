66123155a09ed97e92f3402510f71b6a0b2d5163
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2020-07-09 10:00:46 +0200

    Exercise 14

--- bank-manager-client/src/main/java/org/bank/manager/client/ClientLauncher.java
+++ bank-manager-client/src/main/java/org/bank/manager/client/ClientLauncher.java
@@ -4,6 +4,7 @@ import com.ontimize.db.DatabaseConnectionManager;
 import com.ontimize.gui.ApplicationLauncher;
 import com.ontimize.gui.ApplicationToolBar;
 import com.ontimize.gui.Form;
+import com.ontimize.gui.preferences.BasicApplicationPreferences;
 import com.ontimize.permission.PermissionButton;
 import com.ontimize.util.math.MathExpressionParser;
 import com.ontimize.util.math.MathExpressionParserFactory;
@@ -12,12 +13,14 @@ public class ClientLauncher {
 
 	public static void main(String[] args) {
 		DatabaseConnectionManager.offset = 5;
-	    DatabaseConnectionManager.offsetString = "quickstart";
-		ApplicationToolBar.DEFAULT_BUTTON_SIZE=30;
+		DatabaseConnectionManager.offsetString = "quickstart";
+		ApplicationToolBar.DEFAULT_BUTTON_SIZE = 30;
 		Form.WORD_TEMPLATES = 1;
+		BasicApplicationPreferences.remoteUserPreferences = true;
+		BasicApplicationPreferences.checkOldPreferences = false;
 		PermissionButton.setPropertyFile("org/bank/manager/client/permission/PermissionButton.properties");
 		System.setProperty(MathExpressionParserFactory.MATH_EXPRESSION_PARSER_PROPERTY, MathExpressionParser.MESP);
 		ApplicationLauncher.main(args);
 	}
-	
+
 }
